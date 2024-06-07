ce1b07cdabc7429d7e874bc5c1ebe9c36a5b6fea
ArianCelik <ariancelik@hotmail.com>
2024-06-07 19:13:01 +0200

    cycle working

--- client/src/code/cycle/Cycle.ts
+++ client/src/code/cycle/Cycle.ts
@@ -16,46 +16,48 @@ export class Cycle {
     endDate!: Date;
 
     constructor(cycleType = CycleType.ONE_WEEK, startDate?: Date, endDate?: Date) {
-        this.cycleType = cycleType
+        this.cycleType = cycleType;
 
         if(!startDate) {
-            this.startDate = new Date()
-            this.endDate = addDays(this.startDate, 7)
+            this.startDate = new Date();
+            this.endDate = addDays(this.startDate, 7);
         }
     }
 
-    getCyclePretty = () => {
-        const startDateString = formatDate(this.startDate, "eeee, dd.LL")
-        const endDateString = formatDate(this.endDate, "eeee, dd.LL")
-
-        return startDateString + " to " + endDateString 
+    getCyclePretty(){       
+        const startDateString = formatDate(this.startDate, "eeee, dd.LL");
+        const endDateString = formatDate(this.endDate, "eeee, dd.LL");
+        (document.getElementById("date-area") as HTMLInputElement).value = startDateString + " to " + endDateString ;
     }
 
     goNextCycle = () => {
+        console.log(this.startDate)
         switch(this.cycleType) {
             case CycleType.ONE_WEEK: {
                 this.startDate = addDays(this.startDate, 7)
                 this.endDate = addDays(this.endDate, 7)
-            }
-            case CycleType.TWO_WEEKS:
-            case CycleType.THREE_WEEKS:
-            case CycleType.ONE_MONTH:
-            case CycleType.ONE_YEAR:
+                this.getCyclePretty()
+            }  break 
+            case CycleType.TWO_WEEKS:break
+            case CycleType.THREE_WEEKS:break
+            case CycleType.ONE_MONTH:break
+            case CycleType.ONE_YEAR:break
         }
     }
 
     goPreviousCycle = () => {
 
-        console.log("Hi")
+        
         switch(this.cycleType) {
             case CycleType.ONE_WEEK: {
                 this.startDate = subDays(this.startDate, 7)
                 this.endDate = subDays(this.endDate, 7)
-            }
-            case CycleType.TWO_WEEKS:
-            case CycleType.THREE_WEEKS:
-            case CycleType.ONE_MONTH:
-            case CycleType.ONE_YEAR:
+                this.getCyclePretty()
+            }break
+            case CycleType.TWO_WEEKS:break
+            case CycleType.THREE_WEEKS:break
+            case CycleType.ONE_MONTH:break
+            case CycleType.ONE_YEAR:break
         }
     }
 }
