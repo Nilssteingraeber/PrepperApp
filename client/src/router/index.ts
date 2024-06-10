e07fed7b1c399de72643e4881553afb098ce7bd7
Nils <n-steingraeber@t-online.de>
2024-06-10 16:47:36 +0200

    error fix

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -8,9 +8,6 @@ import ProductsView from '@/views/ProductsView.vue'
 import CalenderView from '@/views/CalenderView.vue'
 import SettingsView from '@/views/SettingsView.vue'
 
-
-const props = 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
