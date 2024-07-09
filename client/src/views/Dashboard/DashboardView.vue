9334f645dcab0db2bd3bae99d1a2aaf5ce706eb7
Akin6761 <142395918+Akin6761@users.noreply.github.com>
2024-07-10 01:47:27 +0200

    okok

--- client/src/views/Dashboard/DashboardView.vue
+++ client/src/views/Dashboard/DashboardView.vue
@@ -2,41 +2,22 @@
 import { reactive } from 'vue'
 
 import store from '@/store'
-import ShoppingList from './ShoppingList.vue';
-import CycleSelectionHeader from '../CycleHeader/CycleSelectionHeader.vue';
-import ShoppingListSelector from './ShoppingListSelector.vue';
-import ShoppingListActions from './ShoppingListActions.vue';
-
-
+import ShoppingList from './ShoppingList.vue'
+import CycleSelectionHeader from '../CycleHeader/CycleSelectionHeader.vue'
+import ShoppingListSelector from './ShoppingListSelector.vue'
+import ShoppingListActions from './ShoppingListActions.vue'
 </script>
 
 <template>
-	<div class="col-12">
-		<div class="row pt-4">
-			<div class="col-8">
-				<ShoppingList></ShoppingList>
-			</div>
-			<div class="col-4">
-				<div class="sticky">
-					<div class="row pb-3">
-						<div class="col-12">
-							<ShoppingListSelector></ShoppingListSelector>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-12">
-							<ShoppingListActions></ShoppingListActions>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="row pt-5">
+    <div class="col-xxl-8">
+      <ShoppingList class="mb-3" />
+    </div>
+    <div class="col-xxl-4">
+      <ShoppingListSelector class="mb-3" />
+      <ShoppingListActions />
+    </div>
+  </div>
 </template>
 
-<style>
-.sticky {
-	position: absolute;
-	margin-right: 2%;
-}
-</style>
+<style></style>
